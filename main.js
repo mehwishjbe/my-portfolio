649f399f64192acfaedc9e1189d679dab3137b75
mehwishjbe <mehwish.jbe@gmail.com>
2023-10-08 16:59:29 +0200

    making h1 in hero section visible and disappear on clicking hamburger

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const hamburgerJS = document.querySelector('.hamburger');
 const navMenuJS = document.querySelector('.navMenu');
+const moveHeading = document.querySelector('.hero-image-h1');
+let headingVisible =  true;
 
 
 hamburgerJS.addEventListener("click", () =>{
 
     hamburgerJS.classList.toggle("active");
     navMenuJS.classList.toggle("active");
+
+    if(headingVisible){
+        moveHeading.style.display = 'none';
+    }else{
+        moveHeading.style.display = 'block';
+    }
+    headingVisible = !headingVisible;
 });
 
 document.querySelectorAll(".nav-link").forEach(ele => 
